9889bb0b3a262d88c833ef04463b9595fdefee25
Вадим Артюшенко <artushenkomail@mail.ru>
2019-10-03 23:13:41 +0700

    add

--- test_3/test_3/test_3/Source_task3.cpp
+++ test_3/test_3/test_3/Source_task3.cpp
@@ -1,33 +1,37 @@
-//для вектора
-#include <vector>
-//для sort и unique
+#include <iostream>
 #include <algorithm>
-using namespace std;
-
-struct Point
-{
-	int x, y, z;
-	Point(int x = 0, int y = 0, int z = 0) : x(x), y(y), z(z) {}
-};
-
-//оператор меньше для sort
-bool operator<(const Point& a, const Point& b)
-{
-	return a.x < b.x || a.x == b.x && (a.y < b.y || a.y == b.y && a.z < b.z);
-}
-//оператор проверки на равенство для unique
-bool operator==(const Point& a, const Point& b)
-{
-	return a.x == b.x && a.y == b.y && a.z == b.z;
-}
-
+#include <vector>
+#include <string>
+#include <cctype>
 
 int main()
 {
-	vector<Point> p;
 
-	sort(p.begin(), p.end());
-	//и удаляем дубликаты
-	p.resize(unique(p.begin(), p.end()) - p.begin());
-	//всё, теперь p содержит только уникальные точки
+	// удаление дубликатов из вектора
+	std::vector<int> v{ 1,2,3,1,2,3,3,4,5,4,5,11,6,7 };
+	std::cout << "Initial vector" << std::endl;
+	for (int i : v)
+		std::cout << i << " ";
+	std::cout << std::endl;
+	std::sort(v.begin(), v.end()); // 1 1 2 2 3 3 3 4 4 5 5 6 7 11
+	for (int i : v)
+		std::cout << i << " ";
+	std::cout << std::endl;
+	auto last = std::unique(v.begin(), v.end());
+	//Удаляет все последовательно повторяющиеся элементы из диапазона [v.begin(), v.end()) и возвращает итератор на 
+	//элемент, следующий за последним элементом нового диапазона. 
+	// v сейчас содержит {1 2 3 4 5 6 7 x x x x x x}, где 'x' обозначает неопределённый элемент
+	v.erase(last, v.end());
+	for (int i : v)
+		std::cout << i << " ";
+	std::cout << std::endl;
+/*
+	// удаление последовательных повторяющихся пробелов
+	std::string s = "wanna go    to      space?";
+	auto end = std::unique(s.begin(), s.end(), [](char l, char r) {
+		return std::isspace(l) && std::isspace(r) && l == r;
+		});
+	// s сейчас содержит "wanna go to space?xxxxxxxx", где 'x' обозначает неопределённый символ
+	std::cout << std::string(s.begin(), end) << '\n';
+	*/
 }
