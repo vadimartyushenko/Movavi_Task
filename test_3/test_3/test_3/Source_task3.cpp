69b0be299c4779f6910541cb370c4c938873df7a
Вадим Артюшенко <artushenkomail@mail.ru>
2019-10-04 15:29:10 +0700

    add_15_28_04

--- test_3/test_3/test_3/Source_task3.cpp
+++ test_3/test_3/test_3/Source_task3.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <string>
-#include <cctype>
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <ctime>
 
 int main()
 {
-
-	// удаление дубликатов из вектора
-	std::vector<int> v{ 1,2,3,1,2,3,3,4,5,4,5,11,6,7 };
+	std::clock_t t;
+	std::mt19937 gen{ static_cast<unsigned>(std::time(nullptr)) };// создание генератора псевдослучайных чисел
+	std::uniform_int_distribution<> dist(1, 10);//раномерное распределение в диапазоне [1, 10]
+	std::vector<int> v(10);
+	std::generate(v.begin(), v.end(), std::bind(dist, gen));//заполняем вектор 
 	std::cout << "Initial vector" << std::endl;
-	for (int i : v)
-		std::cout << i << " ";
-	std::cout << std::endl;
-	std::sort(v.begin(), v.end()); // 1 1 2 2 3 3 3 4 4 5 5 6 7 11
-	for (int i : v)
-		std::cout << i << " ";
-	std::cout << std::endl;
-	auto last = std::unique(v.begin(), v.end());
-	//Удаляет все последовательно повторяющиеся элементы из диапазона [v.begin(), v.end()) и возвращает итератор на 
-	//элемент, следующий за последним элементом нового диапазона. 
-	// v сейчас содержит {1 2 3 4 5 6 7 11 x x x x x x}, где 'x' обозначает неопределённый элемент
-	v.erase(last, v.end());
-	for (int i : v)
-		std::cout << i << " ";
-	std::cout << std::endl;
+	//for (int i : v) std::cout << i << " ";
+	std::cout << "\n-----------------------------------\n";
+	t = clock();
+	std::sort(v.begin(), v.end());
+	v.erase(std::unique(v.begin(), v.end()), v.end());
+	t = clock() - t;
+	std::cout << "Unique vector" << std::endl;
+	//for (int i : v) std::cout << i << " ";
+	std::cout << "\n-----------------------------------\n";
+	std::cout << "Algorithm running time: " << t << " clicks, " << ((double)t / CLOCKS_PER_SEC) << " seconds" << std::endl;
+	return 0;
 }
