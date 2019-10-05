880bc84d006b8974eb2cf3b8bd2ce8ce9ecfdf6b
Вадим Артюшенко <artushenkomail@mail.ru>
2019-10-05 13:19:03 +0700

    13_18_05

--- test_3/test_3/test_3/Source_task3.cpp
+++ test_3/test_3/test_3/Source_task3.cpp
@@ -4,24 +4,25 @@
 #include <functional>
 #include <random>
 #include <ctime>
+#include <chrono>
 
 int main()
 {
-	std::clock_t t;
 	std::mt19937 gen{ static_cast<unsigned>(std::time(nullptr)) };// создание генератора псевдослучайных чисел
-	std::uniform_int_distribution<> dist(1, 10);//раномерное распределение в диапазоне [1, 10]
-	std::vector<int> v(10);
+	std::uniform_int_distribution<> dist(1, 100);//раномерное распределение в диапазоне [1, 10]
+	std::vector<int> v(10000000);
 	std::generate(v.begin(), v.end(), std::bind(dist, gen));//заполняем вектор 
 	std::cout << "Initial vector" << std::endl;
 	//for (int i : v) std::cout << i << " ";
 	std::cout << "\n-----------------------------------\n";
-	t = clock();
+	auto start = std::chrono::high_resolution_clock::now();//замеряем время начала
 	std::sort(v.begin(), v.end());
 	v.erase(std::unique(v.begin(), v.end()), v.end());
-	t = clock() - t;
+	auto end = std::chrono::high_resolution_clock::now();//замеряем время конца
 	std::cout << "Unique vector" << std::endl;
 	//for (int i : v) std::cout << i << " ";
 	std::cout << "\n-----------------------------------\n";
-	std::cout << "Algorithm running time: " << t << " clicks, " << ((double)t / CLOCKS_PER_SEC) << " seconds" << std::endl;
+	std::chrono::duration<float> time_chrono = end - start;
+	std::cout << "Algorithm running time: "<< time_chrono.count() << " seconds" << std::endl;
 	return 0;
 }
